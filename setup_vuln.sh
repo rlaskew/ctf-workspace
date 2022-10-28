b58060169e9f95dc5607a268e77e4b17801fd204
rlaskew <richardaskew@yahoo.com>
2022-10-28 11:39:59 -0500

    Update setup_vuln.sh

--- setup_vuln.sh
+++ setup_vuln.sh
@@ -5,3 +5,9 @@ gcc -w -o vuln1 vuln1.c
 echo "flag found!" > flag.txt
 
 ## Answer: TBD
+
+## other commands
+# python2 -c 'print "\x50"'  ## print P
+# printf '\050'  ## print (
+# printf '\50'  ## print (
+# printf '\x50' ; echo   ## print P plus newline
